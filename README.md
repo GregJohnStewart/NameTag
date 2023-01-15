b9cfcc066e63727a000c99398ad2746954237fef
Greg Stewart <contact@gjstewart.net>
2023-01-14 19:55:56 -0500

    Finished demo with important components

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ### Board setup:
 
  1. Add esp32 board manager URL (preferences -> board manager url's)
- 	- https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+ 	- `https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json`
  2. Open Boards Manager from Tools > Board menu and install `esp32` platform
  3. Select the board: Tools -> Board: * -> ESP32 Arduino -> UM FeatherS3
  4. enable the `CDC on boot` option in the Arduino IDE tools menu
@@ -13,14 +13,28 @@
 
 ### Libraries:
 
+Libraries to setup in 
+
  - `UMS3 Helper`
- - 
+ - `Adafruit EPD`
+ - `adafruit ImageReader`
+ - `adafruit GFX`
 
 ### Writing code to 
+
+ 1. Get board ready
+	 1. press and hold `boot` button
+	 2. press and release `reset`
+	 3. release `boot`
+ 4. Upload code from Arduino Studio. Wait for upload.
+ 5. When done, reset board with board button
 
 ### resources:
 	
  - https://esp32s3.com/getting-started.html
  - https://esp32s3.com/feathers3.html
  - https://docs.espressif.com/projects/arduino-esp32/en/latest/installing.html#linux
+ - https://learn.adafruit.com/adafruit-2-9-eink-display-breakouts-and-featherwings/arduino-setup
+ 
+
  
--- arduinoSketch/NameTag/NameTag.ino
+++ arduinoSketch/NameTag/NameTag.ino
@@ -3,30 +3,97 @@
 
 //CLion
 #include <UMS3.h>
+#include "Adafruit_ThinkInk.h"
+
+#define EPD_DC      3  //10 // can be any pin, but required!
+#define EPD_CS      1  //9  // can be any pin, but required!
+#define SRAM_CS     38 //6  // can set to -1 to not use a pin (uses a lot of RAM!)
+#define EPD_BUSY    -1 //7  // can set to -1 to not use a pin (will wait a fixed delay)
+#define EPD_RESET   -1 //8  // can set to -1 and share with chip Reset (can't deep sleep)
+#define SCREEN_UPDATE_WAIT  30000
 
 UMS3 ums3;
+ThinkInk_290_Tricolor_Z10 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 
 void setup() {
   // Initialize all board peripherals, call this first
   ums3.begin();
+  ums3.setPixelBrightness(255 / 5);
 
-  // Brightness is 0-255. We set it to 1/3 brightness here
-  ums3.setPixelBrightness(255 / 3);
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+  Serial.println("Adafruit EPD full update test in red/black/white");
+  display.begin(THINKINK_TRICOLOR);
 }
 
 int color = 0;
+int lastUpdateTime = -1 * SCREEN_UPDATE_WAIT;
+byte curDemo = 0;
 
 void loop() {
   // colorWheel cycles red, orange, ..., back to red at 256
   ums3.setPixelColor(UMS3::colorWheel(color));
   color++;
-
-  // On the feathers3, toggle the LED twice per cycle
-#ifdef ARDUINO_FEATHERS3
   if (color % 128 == 0) {
     ums3.toggleBlueLED();
   }
-#endif
 
-  delay(15);
+  //Serial.print("Time since last update: ");
+  Serial.println(millis() - lastUpdateTime);
+  if (SCREEN_UPDATE_WAIT < millis() - lastUpdateTime) {
+    if (curDemo == 0) {
+      curDemo++;
+      Serial.println("Banner demo");
+      display.clearBuffer();
+      display.setTextSize(3);
+      display.setCursor((display.width() - 144) / 2, (display.height() - 24) / 2);
+      display.setTextColor(EPD_BLACK);
+      display.print("Tri");
+      display.setTextColor(EPD_RED);
+      display.print("Color");
+      display.display();
+    } else if (curDemo == 1) {
+      curDemo++;
+      Serial.println("Color rectangle demo");
+      display.clearBuffer();
+      display.fillRect(display.width() / 3, 0, display.width() / 3, display.height(), EPD_BLACK);
+      display.fillRect((display.width() * 2) / 3, 0, display.width() / 3, display.height(), EPD_RED);
+      display.display();
+    } else if (curDemo == 2) {
+      curDemo++;
+      Serial.println("Text demo");
+      // large block of text
+      display.clearBuffer();
+      display.setTextSize(1);
+      testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", EPD_BLACK);
+      display.display();
+    } else {
+      curDemo = 0;
+      Serial.println("Lines demo");
+      display.clearBuffer();
+      for (int16_t i = 0; i < display.width(); i += 4) {
+        display.drawLine(0, 0, i, display.height() - 1, EPD_BLACK);
+      }
+
+      for (int16_t i = 0; i < display.height(); i += 4) {
+        display.drawLine(display.width() - 1, 0, 0, i, EPD_RED);
+      }
+      display.display();
+    }
+
+    lastUpdateTime = millis();
+  }
+//  Serial.println("Waiting...");
+//  delay(15000);
+//  Serial.println("Done Waiting.");
+}
+
+
+void testdrawtext(const char *text, uint16_t color) {
+  display.setCursor(0, 0);
+  display.setTextColor(color);
+  display.setTextWrap(true);
+  display.print(text);
 }
